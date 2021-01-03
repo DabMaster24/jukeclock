bc87f81c899d863bc3d0414a1875900fc101f86d
bigyosh <minhansd@yahoo.com>
2021-01-02 16:50:20 -0800

    Added audio files and cleaned up code.  Fixed code crashing if computer cannot find WiFi connection.

--- Clock.py
+++ Clock.py
@@ -11,33 +11,29 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(25, GPIO.IN)
 mixer.init()
 url = "https://www.youtube.com/playlist?list=PLK3jctReHIcET3Vy9TfZ6I-i5v0LH5seL"
-playlist = pafy.get_playlist(url)
-items = playlist["items"]
-song_index=0
-Instance = vlc.Instance()
-player = Instance.media_player_new()
+wificonnected = False
 sounds=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-sounds[0]=mixer.Sound("piano2.wav")
-sounds[1]=mixer.Sound("/home/pi/Clock Project Audio/1.wav")
-sounds[2]=mixer.Sound("/home/pi/Clock Project Audio/2.wav")
-sounds[3]=mixer.Sound("/home/pi/Clock Project Audio/3.wav")
-sounds[4]=mixer.Sound("/home/pi/Clock Project Audio/4.wav")
-sounds[5]=mixer.Sound("/home/pi/Clock Project Audio/5.wav")
-sounds[6]=mixer.Sound("/home/pi/Clock Project Audio/6.wav")
-sounds[7]=mixer.Sound("/home/pi/Clock Project Audio/7.wav")
-sounds[8]=mixer.Sound("/home/pi/Clock Project Audio/8.wav")
-sounds[9]=mixer.Sound("/home/pi/Clock Project Audio/9.wav")
-sounds[10]=mixer.Sound("/home/pi/Clock Project Audio/10.wav")
-sounds[11]=mixer.Sound("/home/pi/Clock Project Audio/lam.wav")
-sounds[12]=mixer.Sound("/home/pi/Clock Project Audio/muoi.wav")
-sounds[13]=mixer.Sound("/home/pi/Clock Project Audio/mot.wav")
-sounds[14]=mixer.Sound("/home/pi/Clock Project Audio/sang.wav")
-sounds[15]=mixer.Sound("/home/pi/Clock Project Audio/trua.wav")
-sounds[16]=mixer.Sound("/home/pi/Clock Project Audio/chieu.wav")
-sounds[17]=mixer.Sound("/home/pi/Clock Project Audio/toi.wav")
-sounds[18]=mixer.Sound("/home/pi/Clock Project Audio/khuya.wav")
-sounds[19]=mixer.Sound("/home/pi/Clock Project Audio/gio.wav")
-sounds[20]=mixer.Sound("/home/pi/Clock Project Audio/phut.wav")
+sounds[0]=mixer.Sound("./Clock_Project_Audio/piano2.wav")
+sounds[1]=mixer.Sound("./Clock_Project_Audio/1.wav")
+sounds[2]=mixer.Sound("./Clock_Project_Audio/2.wav")
+sounds[3]=mixer.Sound("./Clock_Project_Audio/3.wav")
+sounds[4]=mixer.Sound("./Clock_Project_Audio/4.wav")
+sounds[5]=mixer.Sound("./Clock_Project_Audio/5.wav")
+sounds[6]=mixer.Sound("./Clock_Project_Audio/6.wav")
+sounds[7]=mixer.Sound("./Clock_Project_Audio/7.wav")
+sounds[8]=mixer.Sound("./Clock_Project_Audio/8.wav")
+sounds[9]=mixer.Sound("./Clock_Project_Audio/9.wav")
+sounds[10]=mixer.Sound("./Clock_Project_Audio/10.wav")
+sounds[11]=mixer.Sound("./Clock_Project_Audio/lam.wav")
+sounds[12]=mixer.Sound("./Clock_Project_Audio/muoi.wav")
+sounds[13]=mixer.Sound("./Clock_Project_Audio/mot.wav")
+sounds[14]=mixer.Sound("./Clock_Project_Audio/sang.wav")
+sounds[15]=mixer.Sound("./Clock_Project_Audio/trua.wav")
+sounds[16]=mixer.Sound("./Clock_Project_Audio/chieu.wav")
+sounds[17]=mixer.Sound("./Clock_Project_Audio/toi.wav")
+sounds[18]=mixer.Sound("./Clock_Project_Audio/khuya.wav")
+sounds[19]=mixer.Sound("./Clock_Project_Audio/gio.wav")
+sounds[20]=mixer.Sound("./Clock_Project_Audio/phut.wav")
 mixer.music.set_volume(10)
 
 
@@ -132,26 +128,37 @@ def play_song(url):
 def test_clock(channel):
 #     while True:
 #        if not GPIO.input(25):
-    global current_state, song_index, player
+    global current_state, song_index, player, wificonnected
+    
 
     if current_state==state_idle:   
         print("idle")
         get_time()
         current_state=state_time
     elif current_state==state_time:
-        print("time")
         
-        item = items[song_index]
-        i_pafy = item['pafy'] 
-        y_url = i_pafy.watchv_url
-        length = play_song(y_url)
-        
-        if song_index==len(items)-1:
-            song_index=0
-        else:
-            song_index+=1
+        if wificonnected == True:
             
-        current_state=state_song
+            try:
+                print("time")
+                
+                item = items[song_index]
+                i_pafy = item['pafy'] 
+                y_url = i_pafy.watchv_url
+                length = play_song(y_url)
+                
+                if song_index==len(items)-1:
+                    song_index=0
+                else:
+                    song_index+=1
+                    
+            except:
+                print("Error playing song")
+                
+            current_state=state_song
+        else:
+            current_state=state_idle
+            
     elif current_state==state_song:
         print("song")        
         player.stop()
@@ -164,12 +171,23 @@ state_song=2
 current_state=state_idle
 mixer.Sound.play(sounds[0])
 time.sleep(delay)
-GPIO.add_event_detect(25, GPIO.FALLING, callback=test_clock, bouncetime=5000)
+GPIO.add_event_detect(25, GPIO.FALLING, callback=test_clock, bouncetime=6000)
 
 while True: 
 #     try:
 #     GPIO.wait_for_edge(25, GPIO.FALLING)
     time.sleep(delay)
+    
+    if wificonnected == False:
+        try:
+            playlist = pafy.get_playlist(url)
+            items = playlist["items"]
+            song_index=0
+            Instance = vlc.Instance("--no-video")
+            player = Instance.media_player_new()
+            wificonnected = True
+        except:
+            print("Error getting playlist")
 # 
 #     except KeyboardInterrupt:
 #         GPIO.cleanup()
